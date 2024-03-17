9689c50e3f745707a5f2ae3c378d8335a13a6ad9
Sahanaroy1 <sahanaroy20@gmail.com>
2024-03-17 23:27:05 +0000

    refactored badge function

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<h1>dd</h1>
-  <img src="https://img.shields.io/badge/License-MIT-yellow.svg">
+<h1>d</h1>
+  renderLicenseBadge(Apache 2.0)
   <br />
   <br />
   
@@ -16,25 +16,25 @@
   - [Questions](#questions)
   
   ## Installation
-  💾 ddd
+  💾 d
   
   ## Usage
-  💻 dd
+  💻 d
   
   ## License
-  ![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+  ![badge](https://img.shields.io/badge/license-Apache 2.0-brightgreen)
   <br />
-  This application is covered by the MIT license. 
+  This application is covered by the Apache 2.0 license. 
   
   ## Contributing
-  👪 
+  👪 d
   
   ## Tests
-  ✏️ 
+  ✏️ d
   
   ## Questions
+  d<br />
   <br />
+  :octocat: Find me on GitHub: <a href="https://github.com/d" target="_blank">d</a><br />
   <br />
-  :octocat: Find me on GitHub: <a href="https://github.com/" target="_blank"></a><br />
-  <br />
-  ✉️ Email me with any questions: <a href="mailto:" target="_blank"></a><br /><br />
+  ✉️ Email me with any questions: <a href="mailto:d" target="_blank">d</a><br /><br />
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT"){
+    `<img src="https://img.shields.io/badge/License-MIT-yellow.svg">`
+  } else if (license === "Apache 2.0") {
+   `<img src=" https://img.shields.io/badge/License-Apache_2.0-blue.svg">`
+  } else if (license === "GPL 3.0") {
+    `<img src=" https://img.shields.io/badge/License-GPLv3-blue.svg">`
+   } else if (license === "BSD 3") {
+    `<img src=" https://img.shields.io/badge/License-BSD_3--Clause-blue.svg">`
+   } else{
+    `<p>No Liscense found</p>`
+   }
+   
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -16,7 +30,7 @@ function renderLicenseSection(license) {}
 function generateMarkdown(answers) {
 
   return `<h1>${answers.projectTitle}</h1>
-  <img src="https://img.shields.io/badge/License-MIT-yellow.svg">
+  renderLicenseBadge(${answers.license})
   <br />
   <br />
   
